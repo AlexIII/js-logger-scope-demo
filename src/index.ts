f7a169238e5aae8a5af95ee76d46a6dbd5e11f08
Alexander Chernoskutov <alecherov@gmail.com>
2025-05-12 15:17:45 +0400

    Add use(honoLogger)

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import { serve } from '@hono/node-server';
 import { Hono } from 'hono';
+import { logger as honoLogger } from 'hono/logger';
 import { useLoggerRequestContext, logger } from './logger.js';
 
 const app = new Hono();
 
 app.use(useLoggerRequestContext());
+app.use(honoLogger(logger.info.bind(logger)));
 
 app.get('/hello', async (c) => {
   logger.info('Computing...');
